f58d12ae25c7436dacdefab46c804c15c19d49fe
Jorrie Brettin <jorriebrettin@gmail.com>
2015-12-30 21:36:32 -0600

    Initial commit, working hand movement

---
+++ BabyBalance/Assets/Scripts/Hand.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hand : MonoBehaviour {
+
+	public Vector3 handPos;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton(0)) {
+			float normalizedHandPos = gameObject.transform.position.x;
+			float mousePos = (Input.mousePosition.x / Screen.width - 0.5f) * 20;
+			float xPos = gameObject.transform.position.x + (mousePos - normalizedHandPos);
+			handPos = new Vector3 (Mathf.Clamp(xPos,-8,10), -15, 0);
+			gameObject.transform.position = handPos;
+		}
+	}
+}
