a070b64b97e186ed1d764040bb50b4cc1404d0dd
Jorrie Brettin <jorriebrettin@gmail.com>
2016-01-05 20:43:03 -0500

    Working baby and hinge, unpolished

---
+++ BabyBalance/Assets/Resources/Scripts/Baby.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Baby : MonoBehaviour {
+
+	Sprite[] babySprites;
+	SpriteRenderer baby;
+
+	void Start () {
+		baby = gameObject.GetComponent<SpriteRenderer> ();
+		babySprites = Resources.LoadAll<Sprite> ("Art/Babies");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		HingeJoint2D hinge = gameObject.GetComponent<HingeJoint2D>();
+		if (Mathf.Abs (hinge.jointAngle) < 10) {
+			baby.sprite = babySprites [0];
+		} else if (Mathf.Abs (hinge.jointAngle) < 20) {
+			baby.sprite = babySprites [1];
+		} else if (Mathf.Abs (hinge.jointAngle) < 45) {
+			baby.sprite = babySprites [2];
+		} else {
+			hinge.enabled = false;
+			hinge.enableCollision = true;
+		}
+	}
+}
