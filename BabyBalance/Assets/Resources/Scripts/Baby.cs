f44257984a2596d6dc62484afcde73f8dd7453a1
Jorrie Brettin <jorriebrettin@gmail.com>
2016-01-05 20:46:29 -0500

    Baby rotation logic bug fix

--- BabyBalance/Assets/Resources/Scripts/Baby.cs
+++ BabyBalance/Assets/Resources/Scripts/Baby.cs
@@ -14,15 +14,18 @@ public class Baby : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		HingeJoint2D hinge = gameObject.GetComponent<HingeJoint2D>();
-		if (Mathf.Abs (hinge.jointAngle) < 10) {
-			baby.sprite = babySprites [0];
-		} else if (Mathf.Abs (hinge.jointAngle) < 20) {
-			baby.sprite = babySprites [1];
-		} else if (Mathf.Abs (hinge.jointAngle) < 45) {
-			baby.sprite = babySprites [2];
-		} else {
-			hinge.enabled = false;
-			hinge.enableCollision = true;
+		if (hinge.enabled) {
+			if (Mathf.Abs (hinge.jointAngle) < 10) {
+				baby.sprite = babySprites [0];
+			} else if (Mathf.Abs (hinge.jointAngle) < 20) {
+				baby.sprite = babySprites [1];
+			} else if (Mathf.Abs (hinge.jointAngle) < 45) {
+				baby.sprite = babySprites [2];
+			} else {
+				hinge.enabled = false;
+				hinge.enableCollision = true;
+			}
 		}
+
 	}
 }
